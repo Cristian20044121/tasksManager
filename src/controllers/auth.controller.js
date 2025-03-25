c84f4ef7b28ee852badd8bd24b283412944a30a2
Cristian20044121 <crdcaro01@gmail.com>
2025-03-25 10:06:19 -0500

    implementacion de logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -51,7 +51,14 @@ const login = async (req, res) => {
   }
 };
 
+const logout = async (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.status(200).json({ message: "logout" });
+};
 export default {
   register,
   login,
+  logout,
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,4 +4,5 @@ import controllerAuth from "../controllers/auth.controller.js";
 const router = express.Router();
 router.post("/register", controllerAuth.register);
 router.post("/login", controllerAuth.login);
+router.post("/logout", controllerAuth.logout);
 export default router;
